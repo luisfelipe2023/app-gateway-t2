635e2323862ac7a2db80ab4e4e662acefe9637e8
Luis <i202211476@cibertec.edu.pe>
2024-10-12 03:22:54 -0500

    Gateway

---
+++ src/main/java/pe/edu/cibertec/app_gateway_t2/AppGatewayT2Application.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.app_gateway_t2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppGatewayT2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppGatewayT2Application.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/cibertec/app_gateway_t2/AppGatewayT2ApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.app_gateway_t2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppGatewayT2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
